a50028a650b030f5e3c9b6eef7f716b15be965ba
wfy-yr <80297772+wfy-yr@users.noreply.github.com>
2026-07-14 17:03:09 +0800

    Update clk_rst_api.sv

--- clk_rst_api.sv
+++ clk_rst_api.sv
@@ -212,3 +212,176 @@ endtask
 
 
 endclass
+
+//============================================================
+// Reset API
+//============================================================
+
+
+class reset_handle;
+
+
+
+clk_rst_sequencer seq;
+
+
+
+
+function new(
+clk_rst_sequencer seq
+);
+
+
+this.seq=seq;
+
+
+endfunction
+
+
+
+
+
+task assert
+(
+clk_id_e id
+);
+
+
+
+clk_rst_item req;
+
+
+req=new();
+
+
+
+req.cmd=
+RESET_ASSERT;
+
+
+
+req.clk_id=id;
+
+
+
+seq.start_item(req);
+
+
+seq.finish_item(req);
+
+
+
+endtask
+
+
+
+
+
+task release
+(
+clk_id_e id
+);
+
+
+
+clk_rst_item req;
+
+
+req=new();
+
+
+
+req.cmd=
+RESET_RELEASE;
+
+
+
+req.clk_id=id;
+
+
+
+seq.start_item(req);
+
+
+seq.finish_item(req);
+
+
+
+endtask
+
+
+endclass
+
+//============================================================
+// clk_rst_api
+//============================================================
+
+
+class clk_rst_api;
+
+
+
+clk_handle cpu_clk;
+
+
+clk_handle axi_clk;
+
+
+clk_handle apb_clk;
+
+
+clk_handle ddr_clk;
+
+
+
+reset_handle reset;
+
+
+
+
+clk_rst_sequencer seq;
+
+
+
+
+function new(
+clk_rst_sequencer seq
+);
+
+
+
+this.seq=seq;
+
+
+
+cpu_clk=
+new(CPU_CLK,seq);
+
+
+
+axi_clk=
+new(AXI_CLK,seq);
+
+
+
+apb_clk=
+new(APB_CLK,seq);
+
+
+
+ddr_clk=
+new(DDR_CLK,seq);
+
+
+
+
+reset=
+new(seq);
+
+
+
+endfunction
+
+
+
+endclass
